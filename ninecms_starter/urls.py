88e2a0da28217a0ba3925f75151074cdb952975a
Wtower <gkarak@9-dev.com>
2015-11-25 16:59:42 +0200

    Configured urls.py Also updated requirements Migrates into a sqlite db

--- ninecms_starter/urls.py
+++ ninecms_starter/urls.py
@@ -13,9 +13,29 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^robots\.txt/$', TemplateView.as_view(template_name='ninecms/robots.txt', content_type='text/plain')),
 ]
+
+# static files (images, css, javascript, etc.)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # pragma: no cover
+
+# Last: all remaining pass to CMS
+if settings.I18N_URLS:  # pragma: nocover
+    urlpatterns += i18n_patterns(
+        url(r'^', include('ninecms.urls', namespace='ninecms')),
+    )
+else:  # pragma: nocover
+    urlpatterns += [
+        url(r'^', include('ninecms.urls', namespace='ninecms')),
+    ]
