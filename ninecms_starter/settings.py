cb5e535a776554cf0f18c6234801201cbaa34874
Wtower <gkarak@9-dev.com>
2015-11-25 18:23:13 +0200

    Fixed language setting that failed tests, updated requirements

--- ninecms_starter/settings.py
+++ ninecms_starter/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 LANGUAGES = (
     ('en', 'English'),
